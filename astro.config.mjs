e4c131006a4f96d5e06052125bad132c378f6184
aorek <aquerorodriguez@gmail.com>
2026-05-14 08:31:45 +0200

    build(astro): Remove React, sitemap, typography dependencies and simplify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://alvaroquero.dev',
   experimental: {
     contentLayer: true,
   },
   integrations: [
-    react(),
     tailwind({
       applyBaseStyles: false,
     }),
