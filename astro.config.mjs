47e0903ca195ef4f9586e7fd61b24d48aaabdb61
aorek <aquerorodriguez@gmail.com>
2026-05-14 17:50:23 +0200

    chore: Update all depedencies to latest versions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://alvaroquero.dev',
-  experimental: {
-    contentLayer: true,
+
+  vite: {
+    plugins: [tailwindcss()],
   },
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
 });
